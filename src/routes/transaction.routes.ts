99143803721e7a029377a63d6621e814036b3add
Kleiton Marques <kleitonm.marques@gmail.com>
2020-11-14 18:46:19 -0200

    End Application

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -7,13 +7,18 @@ const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
-// transactionRouter.get('/', (request, response) => {
-//   try {
-//     // TODO
-//   } catch (err) {
-//     return response.status(400).json({ error: err.message });
-//   }
-// });
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({
+      transactions,
+      balance,
+    });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
 
 transactionRouter.post('/', (request, response) => {
   try {
